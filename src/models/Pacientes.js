68aa032d317c6ae427f8ec80941c2edaf21c0bd0
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-01-14 12:04:58 -0300

    feat: add patient model

---
+++ src/models/Pacientes.js
@@ -0,0 +1,48 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../db");
+const Plano = require("./Plano");
+
+const Paciente = sequelize.define(
+  "Paciente",
+  {
+    nome: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    dataNascimento: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    endereco: {
+      type: Sequelize.STRING,
+    },
+    telefone: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    genero: {
+      type: Sequelize.STRING,
+    },
+    codigoPlano: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "plano",
+        key: "codigo",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+Paciente.belongsTo(Plano, {
+  foreignKey: "codigoPlano",
+  as: "plano",
+});
+
+module.exports = Paciente;
