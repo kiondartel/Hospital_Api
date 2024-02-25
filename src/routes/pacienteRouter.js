dfbbbb4f848ef4a3ea3a1174c00ef4b8b89952df
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-02-25 19:30:11 -0300

    Refactor: router to group related patient routes using router.route()

--- src/routes/pacienteRouter.js
+++ src/routes/pacienteRouter.js
@@ -4,8 +4,11 @@ const router = express.Router();
 
 const PatientsController = require("../controllers/PacienteControllers");
 
-router.get("/pacientes", PatientsController.getAllPatients);
-router.post("/pacientes", PatientsController.createPatient);
+router
+  .route("/pacientes")
+  .get(PatientsController.getAllPatients)
+  .post(PatientsController.createPatient);
+
 router.delete("/pacientes/:nome", PatientsController.deletePatientByName);
 
 module.exports = router;
