15fd5e83e93976909cf2d708872fc75b62293a86
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-03-10 09:40:54 -0300

    Feat: Add update plan route to PlanosController

--- src/routes/planosRouter.js
+++ src/routes/planosRouter.js
@@ -7,5 +7,6 @@ const PlanosController = require("../controllers/PlanosControllers");
 router.get("/planos", PlanosController.getAllPlanos); // 1º a rota e chamada pelo front chamando getAllPlanos
 router.post("/planos", PlanosController.createPlane);
 router.delete("/planos/:nome", PlanosController.deletePlanoByName);
+router.put("/planos/:codigo", PlanosController.updatePlano);
 
 module.exports = router;
