22666bea751e4122be1e96b3e1218e7497da6944
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-03-14 21:23:35 -0300

    Refactor: PlanosController to use service layer

--- src/controllers/PlanosControllers.js
+++ src/controllers/PlanosControllers.js
@@ -1,12 +1,12 @@
 // controllers/PlanosController.js
-const Plano = require("../models/Plano");
+
+const planosService = require("../service/planosService");
 
 const PlanosController = {
   async getAllPlanos(req, res) {
-    // 2º apos ser chamado ele espera a reposta de findAll de Plano (Model)
     try {
-      const planos = await Plano.findAll(); // ORM gera uma consulta SQL para buscar todos os registros na tabela de planos
-      res.status(201).json(planos);
+      const planos = await planosService.getAllPlanos();
+      res.status(200).json(planos);
     } catch (error) {
       res.status(500).send(error.message);
     }
@@ -14,19 +14,16 @@ const PlanosController = {
 
   async createPlane(req, res) {
     try {
-      const { nome, valor, ativo, procedimentos } = req.body; // Extrai os dados do corpo da requisição
-      // Verifica se os campos obrigatórios estão presentes
+      const { nome, valor, ativo, procedimentos } = req.body;
       if (!nome) {
         return res.status(400).send("Nome é obrigatório");
       }
-      // Cria um novo plano no banco de dados
-      const novoPlano = await Plano.create({
+      const novoPlano = await planosService.createPlano(
         nome,
         valor,
         ativo,
-        procedimentos,
-      });
-      // Retorna o plano criado
+        procedimentos
+      );
       res.status(201).json(novoPlano);
     } catch (error) {
       res.status(500).send(error.message);
@@ -35,14 +32,11 @@ const PlanosController = {
 
   async deletePlanoByName(req, res) {
     try {
-      const nome = req.params.nome; // Captura o nome do plano da URL
-      // Encontra e exclui o plano pelo nome
-      const plano = await Plano.findOne({ where: { nome } });
-      if (!plano) {
+      const { nome } = req.params;
+      const planoExcluido = await planosService.deletePlano(nome);
+      if (!planoExcluido) {
         return res.status(404).send("Plano não encontrado");
       }
-      await plano.destroy();
-
       res.send(`Plano '${nome}' excluído com sucesso.`);
     } catch (error) {
       res.status(500).send(error.message);
@@ -51,23 +45,19 @@ const PlanosController = {
 
   async updatePlano(req, res) {
     try {
-      const codigo = req.params.codigo;
+      const { codigo } = req.params;
       const { nome, valor, ativo, procedimentos } = req.body;
-
-      const plano = await Plano.findByPk(codigo);
-
-      if (!plano) {
+      const planoAtualizado = await planosService.updatePlano(
+        codigo,
+        nome,
+        valor,
+        ativo,
+        procedimentos
+      );
+      if (!planoAtualizado) {
         return res.status(404).send("Plano não encontrado");
       }
-
-      plano.nome = nome || plano.nome;
-      plano.valor = valor || plano.valor;
-      plano.ativo = ativo || plano.ativo;
-      plano.procedimentos = procedimentos || plano.procedimentos;
-
-      await plano.save();
-
-      res.status(200).json(plano);
+      res.status(200).json(planoAtualizado);
     } catch (error) {
       res.status(500).send(error.message);
     }
